94d3624b44c2447ef488ad8ea3958c9b51cfbdfa
Ubuntu <bhat@new.q23ilaledegujd44ymzkb35bxh.frax.internal.cloudapp.net>
2022-12-12 16:17:20 +0000

    First commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         // Write your code here
-        System.out.println("Hello, World!!!");
+        System.out.println("Hello, Changed from local");
     }
 }
